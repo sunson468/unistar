57e3d70daa6f7d79462c865e42a70968ac573c9c
行者 <sunson468@163.com>
2021-03-12 10:30:24 +0800

    扩大Trace保存周期到7天

--- unistar-central/src/main/java/com/up1234567/unistar/central/api/HomeController.java
+++ unistar-central/src/main/java/com/up1234567/unistar/central/api/HomeController.java
@@ -3,14 +3,15 @@ package com.up1234567.unistar.central.api;
 
 import com.up1234567.unistar.central.api.model.BaseInModel;
 import com.up1234567.unistar.central.api.model.BaseOutModel;
+import com.up1234567.unistar.central.api.model.dash.DashMonitorOutModel;
 import com.up1234567.unistar.central.service.connect.impl.UnistarConnectCacheService;
 import com.up1234567.unistar.central.service.stat.impl.StatTraceCacheService;
 import com.up1234567.unistar.central.service.stat.impl.StatTraceService;
+import com.up1234567.unistar.central.service.us.impl.AppService;
 import com.up1234567.unistar.central.support.auth.ARoleAuth;
 import com.up1234567.unistar.central.support.auth.AuthToken;
 import com.up1234567.unistar.central.support.auth.EAuthRole;
-import com.up1234567.unistar.central.api.model.dash.DashMonitorOutModel;
-import com.up1234567.unistar.central.service.us.impl.AppService;
+import com.up1234567.unistar.central.support.core.UnistarProperties;
 import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -32,6 +33,9 @@ public class HomeController {
     @Autowired
     private UnistarConnectCacheService unistarConnectCacheService;
 
+    @Autowired
+    private UnistarProperties unistarProperties;
+
     @PostMapping("dash")
     public DashMonitorOutModel index(@RequestAttribute(AuthToken.REQ_AUTH) AuthToken authToken, @RequestBody BaseInModel inModel) {
         DashMonitorOutModel retModel = new DashMonitorOutModel();
@@ -48,7 +52,7 @@ public class HomeController {
     @PostMapping("dashstat")
     public BaseOutModel dashstat(@RequestAttribute(AuthToken.REQ_AUTH) AuthToken authToken, @RequestBody BaseInModel inModel) {
         BaseOutModel retModel = new BaseOutModel();
-        retModel.setData(CollectionUtils.union(statTraceService.passedDayStatTotals(inModel.getNamespace(), 30), statTraceService.todayStatTotals(inModel.getNamespace())));
+        retModel.setData(CollectionUtils.union(statTraceService.passedDayStatTotals(inModel.getNamespace(), unistarProperties.getClean().getTrace()), statTraceService.todayStatTotals(inModel.getNamespace())));
         retModel.setRetCode(BaseOutModel.RET_OK);
         return retModel;
     }
